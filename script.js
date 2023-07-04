536ae7a506fed3ec4d2e31f378f56d6f4f914de4
Milana <milana.zaborovska@gmail.com>
2023-07-04 18:02:15 +0100

    complete scroll animation

---
+++ script.js
@@ -0,0 +1,12 @@
+let left = document.getElementsByClassName('bi-caret-left-fill')[0];
+let right = document.getElementsByClassName('bi-caret-right-fill')[0];
+let cards = document.getElementsByClassName('cards')[0];
+
+left.addEventListener('click', () => {
+    cards.scrollLeft -= 140;
+})
+
+right.addEventListener('click', () => {
+    cards.scrollLeft += 140;
+})
+
